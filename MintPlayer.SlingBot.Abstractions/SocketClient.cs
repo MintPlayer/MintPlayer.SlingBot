9454e9d10aeaf5230d9ecce6f0f9683eda8e7a72
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2024-08-19 23:07:44 +0200

    Send in slices

--- MintPlayer.SlingBot.Abstractions/SocketClient.cs
+++ MintPlayer.SlingBot.Abstractions/SocketClient.cs
@@ -9,14 +9,24 @@ public class SocketClient
 
     public WebSocket WebSocket { get; }
 
+    const int bufferSize = 2048;
+
     public async Task SendMessage(string message)
     {
         switch (WebSocket.State)
         {
             case WebSocketState.Open:
                 var bytes = Encoding.UTF8.GetBytes(message);
-                var arraySegment = new ArraySegment<byte>(bytes, 0, bytes.Length);
-                await WebSocket.SendAsync(arraySegment, WebSocketMessageType.Text, true, CancellationToken.None);
+                var bytesSent = 0;
+
+                do
+                {
+                    var arraySegment = new ArraySegment<byte>(bytes, bytesSent, bufferSize);
+                    bytesSent += bufferSize;
+                    await WebSocket.SendAsync(arraySegment, WebSocketMessageType.Text, bytesSent >= bytes.Length, CancellationToken.None);
+                }
+                while (bytesSent < bytes.Length);
+
                 break;
             case WebSocketState.Closed:
             case WebSocketState.Aborted:

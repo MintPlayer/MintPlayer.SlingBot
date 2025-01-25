9a9f48751c029f070ca33909439f3d7bccde401f
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-01-25 15:50:37 +0100

    Group files

---
+++ Octokit/MintPlayer.Octokit.Extensions/Requests/PullRequestReviewCommentOnFileCreate.PullRequestReviewCommentOnFile.cs
@@ -0,0 +1,38 @@
+﻿using Octokit;
+
+namespace MintPlayer.Octokit.Extensions.Requests;
+
+public sealed class PullRequestReviewCommentOnFileCreate : RequestParameters
+{
+    /// <summary>Creates a comment on the entire file</summary>
+    /// <param name="body">The text of the comment</param>
+    /// <param name="commitId">The SHA of the commit to comment on</param>
+    /// <param name="path">The relative path of the file to comment on</param>
+    public PullRequestReviewCommentOnFileCreate(string body, string commitId, string path)
+    {
+        Body = body;
+        CommitId = commitId;
+        Path = path;
+    }
+
+    /// <summary>
+    /// The text of the comment.
+    /// </summary>
+    [global::Octokit.Internal.Parameter(Key = "body")]
+    public string Body { get; private set; }
+
+    /// <summary>
+    /// The SHA of the commit to comment on.
+    /// </summary>
+    [global::Octokit.Internal.Parameter(Key = "commit_id")]
+    public string CommitId { get; private set; }
+
+    /// <summary>
+    /// The relative path of the file to comment on.
+    /// </summary>
+    [global::Octokit.Internal.Parameter(Key = "path")]
+    public string Path { get; private set; }
+
+    [global::Octokit.Internal.Parameter(Key = "subject_type")]
+    public ESubjectType SubjectType => ESubjectType.File;
+}
---
+++ Octokit/MintPlayer.Octokit.Extensions/Requests/PullRequestReviewCommentWithSideCreate.PullRequestReviewCommentWithSide.cs
@@ -0,0 +1,49 @@
+﻿namespace MintPlayer.Octokit.Extensions.Requests;
+
+public class PullRequestReviewCommentWithSideCreate : RequestParameters
+{
+	/// <summary>Creates a comment on the specified side of the diff</summary>
+	/// <param name="body">The text of the comment</param>
+	/// <param name="commitId">The SHA of the commit to comment on</param>
+	/// <param name="path">The relative path of the file to comment on</param>
+	/// <param name="line">The line index in the diff to comment on</param>
+	/// <param name="side">On which side does the comment go</param>
+	public PullRequestReviewCommentWithSideCreate(string body, string commitId, string path, int line, EPullRequestReviewCommentSide side)
+	{
+		Body = body;
+		CommitId = commitId;
+		Path = path;
+		Line = line;
+		Side = side;
+	}
+
+	/// <summary>
+	/// The text of the comment.
+	/// </summary>
+	[global::Octokit.Internal.Parameter(Key = "body")]
+	public string Body { get; private set; }
+
+	/// <summary>
+	/// The SHA of the commit to comment on.
+	/// </summary>
+	[global::Octokit.Internal.Parameter(Key = "commit_id")]
+	public string CommitId { get; private set; }
+
+	/// <summary>
+	/// The relative path of the file to comment on.
+	/// </summary>
+	[global::Octokit.Internal.Parameter(Key = "path")]
+	public string Path { get; private set; }
+
+	[global::Octokit.Internal.Parameter(Key = "line")]
+	public int? Line { get; private set; }
+
+	/// <summary>
+	/// The side of the comment.
+	/// </summary>
+	[global::Octokit.Internal.Parameter(Key = "side")]
+	public EPullRequestReviewCommentSide Side { get; private set; }
+
+	//[Octokit.Internal.Parameter(Key = "subject_type")]
+	//public ESubjectType SubjectType => ESubjectType.Line;
+}
